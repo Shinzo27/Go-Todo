c4652bc27f57a123985b69ef97cd5a5a0985c335
Shinzo27 <20bmiit031@gmail.com>
2024-11-07 11:01:58 +0530

    Api

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -51,11 +52,16 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+	}))
+
 	app.Get("/api/getTodo", getTodos) 
 	app.Post("/api/createTodo", createTodo)
 	app.Patch("/api/updateTodo/:id", updateTodo)
 	app.Delete("/api/deleteTodo/:id", deleteTodo)
-
 
 	port := os.Getenv("PORT")
 	if port == "" {
